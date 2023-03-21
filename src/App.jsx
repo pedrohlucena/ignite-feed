c35761c8c72760639d4b627337e22d91525e383f
Pedro <pedrohlucena413@gmail.com>
2023-03-21 15:20:41 -0300

    create Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { Post } from './Post'
 import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
 
 import './global.css'
+import styles from './App.module.css'
 
 export function App() {
 
@@ -9,14 +11,19 @@ export function App() {
     <div>
       <Header />
 
-      <Post
-        author="Pedro H. Lucena"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos nulla ex facilis magnam vel molestias, quod incidunt obcaecati aperiam iure. Aliquid enim itaque laudantium, corrupti libero provident cum? Sed, hic."
-      />
-      <Post
-        author="Lucas Silva"
-        content="Hoje eu almocei uma banana"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Pedro H. Lucena"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos nulla ex facilis magnam vel molestias, quod incidunt obcaecati aperiam iure. Aliquid enim itaque laudantium, corrupti libero provident cum? Sed, hic."
+          />
+          <Post
+            author="Lucas Silva"
+            content="Hoje eu almocei uma banana"
+          />
+        </main>
+      </div>
     </div>
   )
 }
