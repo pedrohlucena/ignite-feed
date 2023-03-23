9b7622ca654e82fa67685a3b5bf6cc7eda3bb38a
Pedro <pedrohlucena413@gmail.com>
2023-03-23 13:50:46 -0300

    create comment component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,5 @@
 import { Avatar } from "./Avatar"
+import { Comment } from "./Comment"
 
 import styles from './Post.module.css'
 
@@ -41,6 +42,12 @@ export function Post() {
                     <button type="submit">Publicar</button>
                 </footer>
             </form>
+
+            <div className={styles.commentList}>
+                <Comment/>
+                <Comment/>
+                <Comment/>
+            </div>
         </article>
     )
 }
